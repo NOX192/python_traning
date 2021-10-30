814ffb3295904b7bf6b3da01a04cbdaa3320b42c
Maxim Ponomarev <NOX192>
2021-10-30 21:54:25 +0300

    #2 Задание №14: Реализовать проверки для всех полей контакта на главной странице

--- test/test_contact_home_page_edit_page.py
+++ test/test_contact_home_page_edit_page.py
@@ -20,6 +20,5 @@ def merge_phones_like_on_home_page(contact):
                               [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
 
 def merge_mails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "", map(lambda x: clear(x),
-                       filter(lambda x: x is not None,
-                              [contact.email, contact.email2, contact.email3]))))
+    return "\n".join(filter(lambda x: x != "", filter(lambda x: x is not None,
+                              [contact.email, contact.email2, contact.email3])))
