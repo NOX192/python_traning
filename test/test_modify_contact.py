5fc3bc1f99512c36df7f3a3579ee47a5fff2b9a0
Maxim Ponomarev <NOX192>
2021-11-11 22:14:36 +0300

    Отключаемые проверки пользовательского интерфейса

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,7 @@
 from model.contact import Contact
 import random
 
-def test_modify_some_contat(app, db):
+def test_modify_some_contat(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="777"))
     old_contacts = db.get_contact_list()
@@ -14,4 +14,5 @@ def test_modify_some_contat(app, db):
     index = new_contacts.index(change_contact)
     old_contacts[index] = change_contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,7 @@
 from model.group import Group
 import random
 
-def test_modify_group_name(app, db):
+def test_modify_group_name(app, db, check_ui):
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="test"))
     old_groups = db.get_group_list()
@@ -14,4 +14,5 @@ def test_modify_group_name(app, db):
     index = new_groups.index(change_group)
     old_groups[index] = change_group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
